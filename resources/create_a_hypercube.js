b2dc575c4e8caedf1026dbaa0ee150a2d1234d09
ahmedsakr <ahmed@sakr.ca>
2018-10-20 08:08:06 -0700

    Added barebone code for testing the hypercube

---
+++ resources/create_a_hypercube.js
@@ -0,0 +1,7 @@
+// https://help.qlik.com/en-US/sense-developer/September2018/Subsystems/APIs/Content/Sense_ClientAPIs/CapabilityAPIs/qlik-interface-interface.htm
+
+// This is the basic definition of a hypercube and it's creation. You must
+// define your dimensions with qDimensions, your measures with qMeasures and
+// your qInitialDataFetch, where qHeight is the number of rows being
+// retrieved and qWidth is the number of columns (usually dimensions + measures)
+
